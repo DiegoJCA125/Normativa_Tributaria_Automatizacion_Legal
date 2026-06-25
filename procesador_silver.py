545b31884a1a807a168d8e671c5c9b3f4554dc13
Diego <diegocarmona125@gmail.com>
2026-06-25 16:50:23 -0500

    Se carga el procesador silver completo

--- procesador_silver.py
+++ procesador_silver.py
@@ -31,3 +31,63 @@ def limpiar_texto(texto_crudo):
 
 # PIPELINE DE EXTRACION 
 
+def ejecutar_pipeline_silver():
+    print("Iniciando procesamiento capa silver (Extraccion y limpieza)")
+
+    # SE escanea la carpeta Bronze donde buscara archivos con extension .pdf
+    archivos_bronze = [f for f in os.listdir(ruta_bronze) if f.lower().endswith(".pdf")]
+
+    # Control d seguridad, lo que hara es que si no encuentra pdfs descargados, se frenara el pipeline con la advertencia
+    if not archivos_bronze:
+        print("No se encontraron archivos PF en 'data_bronze'. Coloca un pdf de prueba para continuar")
+
+    print(f"Se detectaron {len(archivos_bronze)} documentos para procesar")
+    
+    # ciclo principal, se procesaran cada archivo pdf encontrado 1x1
+    for nombre_archivo in archivos_bronze:
+        ruta_pdf = os.path.join(ruta_bronze, nombre_archivo)
+
+        # Se genera el nombre d salida reemplzando la estension ,.pdf por _limpio.txt
+        nombre_salida = nombre_archivo.lower().replace(".pdf", "_limpio.txt")
+        ruta_salida = os.path.join(ruta_silver, nombre_salida)
+
+        # Control de idempotencia: si el archivo ya se proceso en el paso, no lo saltamos
+        if os.path.exists(ruta_salida):
+            print(f"Ya existe el texto extraido para: {nombre_archivo}")
+            continue
+
+        print(f"\n Procesando... {nombre_archivo}...")
+        texto_completo = []
+
+        try:
+            # Abrimos el pdf con pdfplumber de foma segura usando un manejador de contexto (with)
+            with pdfplumber.open(ruta_pdf) as pdf:
+                # Se itera pagina x pagina del documento
+                for i, pagina in enumerate(pdf.pages):
+                    # Se extrae las cadenas de texto plano de la pagina actual
+                    texto_pagina = pagina.extract_text()
+
+                    if texto_pagina:
+                        texto_completo.append(texto_pagina)
+                    else:
+                        # Si se retorna None o vacio, se alerta (puede que la pagina sea un escaneo/ imagen pesada)
+                        print(f"   Advertencia: Pagina {i+1} no retorno texto plano (Es un escaneo sin OCR?)")         
+
+            # Se une todo el texto de las paginas y le aplicamos el motor de limpieza
+            texto_final = limpiar_texto("\n".join(texto_completo))            
+
+            if texto_final:
+                #Se guarda el reusltado en la capa silver
+                # Se obliga que sea utf-8 oara guardar acentos
+                with open (ruta_salida, "w", encoding="utf-8") as txt_file:
+                    txt_file.write(texto_final)
+                print(f"Texto guardado con exito en Capa Silver: {nombre_salida}")
+            else:
+                print(f"No se pudo extraer texto legible de: {nombre_archivo}")
+
+        except Exception as e:
+            # Se captura fallos de lectura, archivos danados 
+            print(f"Error critico al procesar el archivo {nombre_archivo}: {e}")
+
+if __name__ == '__main__':
+    ejecutar_pipeline_silver()     
